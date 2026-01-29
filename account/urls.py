69944a3925c4de8946af7a0c4961b0cf7193c8cc
bt-hiba <hibabity10@gmail.com>
2026-01-29 23:23:40 +0100

    Forget and reset password

--- account/urls.py
+++ account/urls.py
@@ -11,5 +11,12 @@ urlpatterns = [
     # User profile
     path('profile/', views.profile, name='profile'),
     path('profile/update/', views.update_profile, name='update_profile'),
+    path('profile/change-password/', views.change_password, name='change_password'),
+    
+    # RESET PASSWORD
+    path('profile/forget-password/', views.request_password_reset, name='request_password_reset'),
+    path('profile/reset-password/<int:uid>/<str:token>/', views.reset_password, name='reset_password'),
 ]
+
+
 
